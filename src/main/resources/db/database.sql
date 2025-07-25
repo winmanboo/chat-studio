1eb56354892f9a33373c0c175b5e909b7994d5db
winmanboo <winmanboo2332@gmail.com>
2025-07-25 17:09:22 +0800

    更新数据表

--- src/main/resources/db/database.sql
+++ src/main/resources/db/database.sql
@@ -24,11 +24,32 @@ create table kb_store
 
 create table user
 (
-    user_id      varchar(64) primary key,
-    username     varchar(24)  not null comment '用户名',
-    nick_name    varchar(24)  not null comment '昵称',
-    password     varchar(128) not null comment '密码',
-    capacity     int          not null default -1 comment '容量 -1无限制',
-    created_time datetime     not null default current_timestamp,
-    updated_time datetime     not null default current_timestamp on update current_timestamp
+    user_id            varchar(64) primary key,
+    email              varchar(36)  not null comment '邮箱',
+    nick_name          varchar(24)  not null comment '昵称',
+    pwd_hash           varchar(255) not null comment '密码',
+    state              varchar(64)  not null comment '用户状态（INIT、ACTIVE、FROZEN）',
+    invite_code        varchar(255)          default null comment '邀请码',
+    inviter_id         varchar(255)          default null comment '邀请人id',
+    capacity           int          not null default -1 comment '容量 -1无限制',
+    profile_avatar_url varchar(255)          default null comment '用户头像地址',
+    deleted            int                   default 0 comment '逻辑删除字段 0-未删除 1-已删除',
+    created_time       datetime     not null default current_timestamp,
+    updated_time       datetime     not null default current_timestamp on update current_timestamp
 ) comment '用户信息';
+
+create table notice
+(
+    id              bigint unsigned not null primary key auto_increment comment 'id',
+    created_time    datetime        not null default current_timestamp,
+    updated_time    datetime        not null default current_timestamp on update current_timestamp,
+    notice_title    varchar(512)    not null comment '通知标题',
+    notice_content  text                     default null comment '通知内容',
+    notice_type     varchar(128)    not null comment '通知类型',
+    send_time       datetime        not null default current_timestamp comment '发送时间',
+    receive_address varchar(256)             default null comment '接收地址',
+    state           varchar(128)    not null comment '状态（INIT、SUCCESS、FAILED、SUSPEND）',
+    deleted         int                      default 0 comment '逻辑删除字段 0-未删除 1-已删除',
+    retry_times     int                      default 0 comment '重试次数',
+    extend_info     varchar(1024)            default null comment '扩展信息'
+)
